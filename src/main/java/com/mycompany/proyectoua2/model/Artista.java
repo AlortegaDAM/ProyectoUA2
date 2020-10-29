1d6ae25d8bcb426fd65271df5890753a2f3fc6b7
Vinil <Vinil@DESKTOP-TBA4D08>
2020-10-29 11:15:06 +0100

    -prueba

---
+++ src/main/java/com/mycompany/proyectoua2/model/Artista.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectoua2.model;
+
+/**
+ *
+ * @author Vinil
+ */
+public class Artista {
+    
+}
